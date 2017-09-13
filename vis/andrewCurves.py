a505805f8d40885c36b70779d7f204b8d2692645
sunkanggao <sunkanggao1992@gmail.com>
2017-09-13 23:58:06 +0800

    andrewCurves

--- vis/andrewCurves.py
+++ vis/andrewCurves.py
@@ -1,12 +1,14 @@
 from sklearn import datasets
 import pandas as pd
 from pandas.tools.plotting import andrews_curves
+import matplotlib.pyplot as plt
+
 
 ir = datasets.load_iris()
 iris = pd.DataFrame(ir.data, columns=list(ir.feature_names))
 iris['species'] = ir.target
 
-color_map = dict(zip(iris.species.unique(), ['blue', 'green', 'red']))
-
-
-andrews_curves(iris, 'species')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax = andrews_curves(iris, 'species', colormap='winter')
+plt.show()
